dfa15696cf85b5df7dc7ec9b03e59534a0d12c4c
LBisca <lucas.simon.b@gmail.com>
2018-08-01 18:12:06 -0300

    Add quantity button

--- app/src/main/java/lucas/com/grocery/view/AddItemAdapter.kt
+++ app/src/main/java/lucas/com/grocery/view/AddItemAdapter.kt
@@ -5,7 +5,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewParent
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import kotlinx.android.synthetic.main.add_item_adapter.view.*
@@ -20,13 +20,14 @@ class AddItemAdapter(
     class AddItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val ivIcon: ImageView = view.iv_product
         val tvTitle: TextView = view.tv_descr
-
+        val up: ImageView = view.button_plus
+        val down: ImageView = view.button_minus
+        val count: TextView = view.count
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, p1: Int): AddItemViewHolder {
         return AddItemViewHolder(LayoutInflater.from(parent.context)
                 .inflate(R.layout.add_item_adapter, parent, false))
-
     }
 
     override fun getItemCount(): Int = items.size
@@ -35,5 +36,14 @@ class AddItemAdapter(
         val item = items[index]
         holder.ivIcon.setImageResource(item.image)
         holder.tvTitle.text = item.name
+        var count = 0
+        holder.up.setOnClickListener {
+            count = count + 1
+            holder.count.text = (count).toString()
+        }
+        holder.down.setOnClickListener {
+            if (count > 0) { count = count - 1 }
+            holder.count.text = (count).toString()
+        }
     }
 }
